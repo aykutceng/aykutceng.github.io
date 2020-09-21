3494e6cc03ae9e2eb962c09dd3f65b8c0444dd79
Aykut Cengiz <49622855+aykutceng@users.noreply.github.com>
2020-09-21 18:52:39 +0300

    Add files via upload

--- Mobilya/rezColor.js
+++ Mobilya/rezColor.js
@@ -41,13 +41,37 @@ $(document).ready(function(){
  });
 
 
- $("#send").click(function() {
- 	var imgs = $(".row .res img");
+ $(".send").click(function() {
+ 	var imgs = $(this).parent();
+ 	if(imgs.attr("id") == '1'){
+ 	$('div#1').addClass("row");
+ 	$('div#1').removeClass("rowShow");}
+ 	if(imgs.attr("id") == '2'){
+ 	$('div#2').addClass("row");
+ 	$('div#2').removeClass("rowShow");}
+ 	imgs = imgs.find("img")
  	var alerting = "";
  	imgs.each(function() {
  		alerting += $(this).attr("src") + ": " + $(this).parent().css("background-color") + "\n"
  	})
  	alert(alerting);
  })
+
+$(".mainImg img").click(function() {
+	var id = $(this).attr("class");
+	if (id == '1') {
+
+		$('div#1').removeClass("row");
+		$('div#1').addClass("rowShow");
+	}
+
+	else if (id == '2') {
+
+		$('div#2').removeClass("row");
+		$('div#2').addClass("rowShow");
+	}
+	
+
+})
 
 });
